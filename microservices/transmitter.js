833540b9d6c696bcebde90e901b818a168ed63d2
root <root@lusi.net>
2023-01-18 16:15:24 +0300

    Temp

--- microservices/transmitter.js
+++ microservices/transmitter.js
@@ -1,69 +1,24 @@
 const CONFIG = require('./config')
 const passport = require('./passport')
-const fs = require('fs')
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({port: CONFIG.ws_port})
 const router = require('./router')
-
 const chalk = require('chalk')
+
 const jsonSize = string => chalk.green.bold((Buffer.byteLength(string, 'utf8') / 1024).toFixed(2) + ' Kb.')
 
 const frameType = frame => {
     let obj = JSON.parse(frame)
-    if (obj.type === 'instruction'  && obj.model.mutations && obj.model.mutations.length) {
+    if (obj.type === 'instruction' && obj.model.mutations && obj.model.mutations.length) {
         return chalk.red.bold(`Instruction ${obj.model.mutations.map(item => item.name).join(', ')}`)
     }
     return chalk.red.bold(obj.type)
 }
 
-const options = {
-    key: fs.readFileSync('/var/www/httpd-cert/www-root/crmkin.com.key'),
-    cert: fs.readFileSync('/var/www/httpd-cert/www-root/crmkin.com.crt')
-}
-
-
-const {createServer} = require('https')
-const {createServerFrom} = require('wss')
-const https = createServer(options)
-
-const wss = createServerFrom(https, ws => {
-    ws.on('message', message => {
-        console.log(chalk.green.bold('Received message =>'), chalk.cyan.bold(`${message}`))
-        checkToInternalFunction({message, ws})
-        router.parse(message)
-            .then(res => {
-                res.response ? console.log(chalk.blue.bold('Sending response'), frameType(res.response), jsonSize(res.response)) : null
-                res.broadcast ? console.log(chalk.blue.bold('Sending broadcast'), frameType(res.broadcast), jsonSize(res.broadcast)) : null
-                res.response ? ws.send(res.response) : null
-                res.broadcast ? broadcast(res.broadcast) : null
-            })
-            .catch(e => console.error(e))
-    })
-})
-https.listen(CONFIG.ws_port)
-
-
-wss.on('connection', ws => {
-    const parseCookie = str => {
-        return {
-            key: str.split('=')[0],
-            value: str.split('=')[1]
-        }
-    }
-    if (!ws.upgradeReq.headers.cookie) {
-        ws.send('Access denied')
-        ws.close()
-    } else {
-        let cookies = ws.upgradeReq.headers.cookie.split(';')
-        cookies = cookies.map(item => parseCookie(item))
-        !passport.verifyToken(cookies) ? ws.close('401', 'Access denied') : console.log('Connected')
-    }
-})
-
-
 const broadcast = data => {
     wss.clients.forEach(client => {
-        client.readyState === 1 ? client.send(data) : null
+        client.readyState === WebSocket.OPEN ? client.send(data) : null
     })
-    console.log(`Delivered to ${[... wss.clients].filter(client => client.readyState === 1).length} clients`)
 }
 
 const activeClients = () => {
@@ -107,7 +62,37 @@ const checkToInternalFunction = ({message, ws}) => {
     }
 }
 
+wss.on('connection', (ws, req) => {
+    const parseCookie = str => {
+        return {
+            key: str.split('=')[0],
+            value: str.split('=')[1]
+        }
+    }
+    if (!req || !req.headers || !req.headers.cookie) {
+        ws.send('Access denied')
+        ws.close()
+    } else {
+        let cookies = req.headers.cookie.split(';')
+        cookies = cookies.map(item => parseCookie(item))
+        !passport.verifyToken(cookies) ? ws.close('401', 'Access denied') :
+            console.log(chalk.green.bold.inverse('Connected from: '), chalk.bold.green(req.socket.remoteAddress))
+    }
 
+    ws.on('message', message => {
+        console.log(chalk.green.bold('Received message =>'), chalk.cyan.bold(`${message}`))
+        checkToInternalFunction({message, ws})
+        router.parse(message)
+            .then(res => {
+                res.response ? console.log(chalk.blue.bold('Sending response'), frameType(res.response), jsonSize(res.response)) : null
+                res.broadcast ? console.log(chalk.blue.bold('Sending broadcast'), frameType(res.broadcast), jsonSize(res.broadcast)) : null
+                res.response ? ws.send(res.response) : null
+                res.broadcast ? broadcast(res.broadcast) : null
+            })
+            .catch(e => console.error(e))
+    })
+})
 
-
-exports.broadcast = broadcast
+module.exports = {
+    broadcast
+}
